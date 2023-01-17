1cadded55aae44065cf58724080a634c7da9f5b4
AnaMach09 <machavarianiana0@gmail.com>
2023-01-18 00:40:59 +0400

    3davaleba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+const generateUsers = () => {
+  return [
+    {id: 1 , name: 'sergi'},
+    {id: 2 , name: 'ana'},
+    {id: 3 , name: 'mari'},
+    {id: 4 , name: 'lasha'},
+    {id: 1 , name: 'giorgi'},
+    {id: 2 , name: 'ana'},
+    {id: 3 , name: 'mari'},
+    {id: 4 , name: 'irakli'}
+  ]
+  
+ }
 
-function App() {
+
+export const App = () => {
+ const [users, setUsers] = useState([]);
+ 
+
+ 
+ useEffect(() => {
+  console.log('generateUsers' , generateUsers());
+  const userFromBe = generateUsers();
+  setUsers(userFromBe)
+ },[]);
+ console.log("users", users);
+
+
+ useEffect(() => {
+  document.title = `${users.length} users left`;
+ },[users]);
+ console.log("users", users);
+ const removeUser = () => {
+  setUsers((prevUsers) => {
+    const index = Math.floor(Math.random() * prevUsers.length)
+    const newUserArray = prevUsers.filter((_, ind) => ind !== index)
+    return newUserArray;
+  });
+ };
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={removeUser}>remove users</button>
+     {users.map(({ id, name}) => (
+      <h1 key={id}>{name}</h1>
+     )
+     )}
     </div>
   );
 }
